7fac5ab1d3599254837dfb42470dd5fc3fbc2eb2
Dave Edwards <davidjonedwardsmusic@gmail.com>
2021-06-02 14:33:52 +0100

    'deploy'

---
+++ precache-manifest.64f86d42aea6bfc56a6cda1fb1dc0a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2971ab54ff3d116e0fea",
+    "url": "/css/app.6f374ef9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "60d5b725d6098694351ab07f530c0d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2971ab54ff3d116e0fea",
+    "url": "/js/app.e001614d.js"
+  },
+  {
+    "revision": "1d30028313dbdd1cca6d",
+    "url": "/js/chunk-vendors.1636ee5c.js"
+  },
+  {
+    "revision": "955ee3a99a75a73c11bb9eb0a0871907",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
